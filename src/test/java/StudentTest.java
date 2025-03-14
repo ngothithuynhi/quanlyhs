c0f855ba7e0e87aa78427c67619eb5e4631d99f0
ngothithuynhi <momonhi2005@gmail.com>
2025-03-14 08:49:11 +0700

    unit test

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -9,6 +9,7 @@
  */
 import com.mycompany.student.Student;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -16,20 +17,30 @@ public class StudentTest {
 
     private Student student;
 
-    @BeforeMethod
+    // Trước khi chạy các test case, tạo đối tượng Student
+    @BeforeClass
     public void setUp() {
-        student = new Student("123", "Minh", 21);
+        student = new Student("12345", "Nguyen Van A", 20); // Tạo một đối tượng Student với thông tin giả
     }
 
+    // Kiểm tra phương thức getName()
     @Test
     public void testGetName() {
-        Assert.assertEquals(student.getName(), "Minh");
+        Assert.assertEquals(student.getName(), "Nguyen Van A", "Loi ten");
     }
 
+    // Kiểm tra phương thức setAge()
     @Test
     public void testSetAge() {
-        student.setAge(22);
-        Assert.assertEquals(student.getAge(), 22);
+        student.setAge(25);
+        Assert.assertEquals(student.getAge(), 25, "Loi tuoi");
+    }
+
+    // Kiểm tra phương thức updateName()
+    @Test
+    public void testUpdateName() {
+        student.updateName("Nguyen Thi B");
+        Assert.assertEquals(student.getName(), "Nguyen Thi B", "Cap nhap ten loi!");
     }
 }
 
